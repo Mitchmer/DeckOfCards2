a0204ce490eaaba5a16e9f2370f3a435725ef10f
Mitch Merrell <mitchmerrell@Mitchs-MacBook-Pro.local>
2019-08-12 11:01:29 -0600

    changed folder structure, set up view controller

---
+++ DeckOfCards/View Controllers/DrawViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DrawViewController.swift
+//  DeckOfCards
+//
+//  Created by Mitch Merrell on 8/12/19.
+//  Copyright © 2019 Mitch Merrell. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func drawButtonTapped(_ sender: Any) {
+    }
+    
+}
